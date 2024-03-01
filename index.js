bb10fbfe0cf0442250a4d146407dec85f23bc9f5
Ayoubxc <153134513+Ayoubxc@users.noreply.github.com>
2024-03-01 07:54:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,12 +24,6 @@ app.get('/text-to-speech', async (req, res) => {
 
     const output = await replicate.run(
       'lucataco/xtts-v2:684bc3855b37866c0c65add2ff39c78f3dea3f4ff103a436465326e0f438d55e',
-      {
-        input: {
-          prompt,
-          speaker,
-          language,
-          cleanup_voice: cleanup_voice === 'true',
       { input: { prompt } }
     );
 
