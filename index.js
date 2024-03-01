78a179440e903a15de492c2f4f7254a2c17e5ca1
Ayoubxc <153134513+Ayoubxc@users.noreply.github.com>
2024-03-01 07:44:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,32 +7,33 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-const replicate = new Replicate({ auth: "r8_6yGN7OMnvt84GFFL03hsS0QIZPT76wX3IpKef" });
+const replicate = new Replicate({   auth: "r8_6yGN7OMnvt84GFFL03hsS0QIZPT76wX3IpKef",
+});
 
 app.use(express.json());
 
-app.get('/', async (req, res) => {
+app.get('/text-to-speech', async (req, res) => {
   try {
-    const { text, language } = req.query;
+    const { prompt } = req.query;
 
-    if (!text) {
-      return res.status(400).json({ error: 'Text is required' });
+    if (!prompt) {
+      return res.status(400).json({ error: 'Prompt is required' });
     }
 
-    console.log(`Received text: ${text}`);
+    console.log(`Received prompt: ${prompt}`);
 
     const output = await replicate.run(
       'lucataco/xtts-v2:684bc3855b37866c0c65add2ff39c78f3dea3f4ff103a436465326e0f438d55e',
       {
         input: {
-          text,
+          prompt,
+          speaker,
           language,
-          cleanup_voice: false,
-        },
-      }
+          cleanup_voice: cleanup_voice === 'true',
+      { input: { prompt } }
     );
 
-    console.log(`Response for text "${text}":`, output);
+    console.log(`Response for prompt "${prompt}":`, output);
 
     return res.json({ result: output });
   } catch (error) {
