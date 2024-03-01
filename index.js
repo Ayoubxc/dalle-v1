ffaf4667bf3f4b2ac7cedefdbe4c2277d2e8de39
Ayoubxc <153134513+Ayoubxc@users.noreply.github.com>
2024-03-01 17:50:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,33 @@
 const express = require('express');
 const Replicate = require('replicate');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
-
-const replicate = new Replicate({   auth: "r8_6yGN7OMnvt84GFFL03hsS0QIZPT76wX3IpKef",
+const replicate = new Replicate({
+  auth: "r8_6yGN7OMnvt84GFFL03hsS0QIZPT76wX3IpKef",
 });
 
-app.use(express.json());
-
-app.get('/text-to-speech', async (req, res) => {
+app.post('/generate-audio', async (req, res) => {
   try {
-    const { prompt } = req.query;
-
-    if (!prompt) {
-      return res.status(400).json({ error: 'Prompt is required' });
-    }
-
-    console.log(`Received prompt: ${prompt}`);
-
+    const { text, speaker, language, cleanup_voice } = req.body;
     const output = await replicate.run(
       'lucataco/xtts-v2:684bc3855b37866c0c65add2ff39c78f3dea3f4ff103a436465326e0f438d55e',
-      { input: { prompt } }
+      {
+        input: {
+          text,
+          speaker,
+          language,
+          cleanup_voice,
+        },
+      }
     );
-
-    console.log(`Response for prompt "${prompt}":`, output);
-
-    return res.json({ result: output });
+    res.json(output);
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: 'Internal server error' });
+    console.error('Error generating audio:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`الخادم يعمل على المنفذ ${PORT}`);
 });
