e85550310d2668031a6cd7c7b1884406b54c42a0
Ayoubxc <153134513+Ayoubxc@users.noreply.github.com>
2024-03-03 06:37:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
 const express = require('express');
 const Replicate = require('replicate');
-const url = require('url');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 const replicate = new Replicate({
   auth: "r8_6yGN7OMnvt84GFFL03hsS0QIZPT76wX3IpKef",
 });
-
-// معالجة طلبات favicon.ico
-app.get('/favicon.ico', (req, res) => res.status(204));
-
-app.get('/generate-audio', async (req, res) => {
+app.get('/synthesize', async (req, res) => {
   try {
-    const queryObject = url.parse(req.url,true).query;
-    const text = queryObject.text;
     const output = await replicate.run(
-      'lucataco/xtts-v2:684bc3855b37866c0c65add2ff39c78f3dea3f4ff103a436465326e0f438d55e',
+      "lucataco/xtts-v2:684bc3855b37866c0c65add2ff39c78f3dea3f4ff103a436465326e0f438d55e",
       {
         input: {
-          text,
-          language: "en",
-          cleanup_voice: false
-        },
+          text: req.query.text,
+          speaker: req.query.speaker,
+          language: req.query.language,
+          cleanup_voice: req.query.cleanup_voice
+        }
       }
     );
     res.json(output);
   } catch (error) {
-    console.error('Error generating audio:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: error.message });
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`الخادم يعمل على المنفذ ${PORT}`);
+app.listen(3000, () => {
+  console.log('Express server is running on port 3000');
 });
