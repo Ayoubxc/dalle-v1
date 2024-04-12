310065a25476919e2fcc21ffe787c4d1e31d28f9
Ayoubxc <153134513+Ayoubxc@users.noreply.github.com>
2024-04-12 14:29:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const Replicate = require('replicate');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const app = express();
 const replicate = new Replicate({
-  auth: r8_VNqij5t59t9qpapPvwzpIuMNLbuyCxc3zgESG, // تحديد المفتاح من متغير البيئة
+  auth: r8_VNqij5t59t9qpapPvwzpIuMNLbuyCxc3zgESG, // اسم متغير البيئة المحدد في Heroku
 });
 
 const PORT = process.env.PORT || 3000;
